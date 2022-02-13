1de3ee5fee47c7361832f3ec7b0b0241ed2a1009
Gabri <58130127+mrgambero@users.noreply.github.com>
2022-02-12 21:46:49 -0700

    Add files via upload

---
+++ prokaryotes_pathways.R
@@ -0,0 +1,16 @@
+library(KEGGREST)
+org <- data.frame(keggList("organism"))
+prokaryotes = org[grep("Prokaryotes", org$phylogeny), ]
+pathways_tot = vector()
+for (i in 3651:length(prokaryotes$organism))
+{
+  try({
+    pathways = keggLink("pathway", prokaryotes[i,2])
+    pathways = sub(paste(".*",prokaryotes[i,2], sep = ""), "", pathways)  
+    pathways = unique(pathways)
+    pathways_tot = append(pathways_tot,pathways)
+    pathways_tot = unique(pathways_tot) })
+}
+pathways_tot
+#write.table(pathways_tot, "pathways_prok.txt")
+
